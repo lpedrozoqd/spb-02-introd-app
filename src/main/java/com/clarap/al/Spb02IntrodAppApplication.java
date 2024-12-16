4a91da33cda2e907cde4b8018a51389fe7a94d13
lpedrozoqd <leonel.pedrozo.quiroz.d@gmail.com>
2024-12-15 21:25:05 -0500

    Carga inicial de archivos

---
+++ src/main/java/com/clarap/al/Spb02IntrodAppApplication.java
@@ -0,0 +1,13 @@
+package com.clarap.al;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spb02IntrodAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spb02IntrodAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clarap/al/Spb02IntrodAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarap.al;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spb02IntrodAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
